e53df7a18de7869bc4f705d0ac6319d5ba29cc0e
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-04-03 12:03:43 +0200

    Test 2. Added test sources to have 100% line and branche coverage

--- src/test/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/GildedRoseTest.kt
@@ -10,6 +10,7 @@ class GildedRoseTest {
     @ParameterizedTest
     @MethodSource("createItems")
     fun `parametrized test for update quality`(actual: Item, expected: Item) {
+        // Launch this test as "Run '...' with Coverage"
         val updatedItem = getUpdatedItem(actual)
 
         assertEquals(expected.toString(), updatedItem.toString(), "Item '${actual.name}' was not updated properly")
@@ -18,8 +19,16 @@ class GildedRoseTest {
     companion object {
         @JvmStatic
         fun createItems() = listOf(
-            // TODO Here we will write our test cases parameters
-            Arguments.of(Item("foo", 1, 0), Item("foo", 0, 0)),
+            Arguments.of(Item("Sulfuras, Hand of Ragnaros", 0, 80), Item("Sulfuras, Hand of Ragnaros", 0, 80)),
+            Arguments.of(Item("Sulfuras, Hand of Ragnaros", -1, 80), Item("Sulfuras, Hand of Ragnaros", -1, 80)),
+            Arguments.of(Item("Aged Brie", 2, 0), Item("Aged Brie", 1, 1)),
+            Arguments.of(Item("Aged Brie", 0, 0), Item("Aged Brie", -1, 2)),
+            Arguments.of(Item("Aged Brie", 0, 51), Item("Aged Brie", -1, 51)),
+            Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20), Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)),
+            Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 20, 20), Item("Backstage passes to a TAFKAL80ETC concert", 19, 21)),
+            Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 5, 49), Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)),
+            Arguments.of(Item("foo", 0, 2), Item("foo", -1, 0)),
+            Arguments.of(Item("foo", 0, 0), Item("foo", -1, 0))
         )
 
         fun getUpdatedItem(actual: Item): Item {
