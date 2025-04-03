a9b79aaa2c83651e895dccbd891764da82df566d
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-04-03 11:57:55 +0200

    Test 1. Implemented parametrized test function

--- src/test/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/GildedRoseTest.kt
@@ -5,5 +5,28 @@ import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class GildedRoseTest
+class GildedRoseTest {
 
+    @ParameterizedTest
+    @MethodSource("createItems")
+    fun `parametrized test for update quality`(actual: Item, expected: Item) {
+        val updatedItem = getUpdatedItem(actual)
+
+        assertEquals(expected.toString(), updatedItem.toString(), "Item '${actual.name}' was not updated properly")
+    }
+
+    companion object {
+        @JvmStatic
+        fun createItems() = listOf(
+            // TODO Here we will write our test cases parameters
+            Arguments.of(Item("foo", 1, 0), Item("foo", 0, 0)),
+        )
+
+        fun getUpdatedItem(actual: Item): Item {
+            val app = GildedRose(listOf(actual))
+            app.updateQuality()
+            return app.items[0]
+        }
+    }
+
+}
