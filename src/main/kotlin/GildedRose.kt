cbb4fac932f5af9b4ce5775e7ded9686b2b9eb87
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-04-03 13:11:21 +0200

    Refactoring 9. 'if' condition swapped with 'when' condition. Added TODO

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -2,31 +2,33 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         items.stream().forEach { item ->
-            if (item.name == "Aged Brie") {
-                increaseQuality(item)
-            } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                increaseQuality(item)
-                if (item.sellIn < 11) {
+            // TODO next we will implement object updaters that we will later use in a factory method
+            when (item.name) {
+                "Aged Brie" -> increaseQuality(item)
+                "Backstage passes to a TAFKAL80ETC concert" -> {
                     increaseQuality(item)
+                    if (item.sellIn < 11) {
+                        increaseQuality(item)
+                    }
+                    if (item.sellIn < 6) {
+                        increaseQuality(item)
+                    }
                 }
-                if (item.sellIn < 6) {
-                    increaseQuality(item)
-                }
-            } else if (item.name != "Sulfuras, Hand of Ragnaros") {
-                decreaseQuality(item)
+                "Sulfuras, Hand of Ragnaros" -> {}
+                else -> decreaseQuality(item)
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn -= 1
+            when (item.name) {
+                "Sulfuras, Hand of Ragnaros" -> {}
+                else -> item.sellIn -= 1
             }
 
             if (item.sellIn < 0) {
-                if (item.name == "Aged Brie") {
-                    increaseQuality(item)
-                } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        item.quality = 0
-                } else if (item.name != "Sulfuras, Hand of Ragnaros") {
-                    decreaseQuality(item)
+                when (item.name) {
+                    "Aged Brie" -> increaseQuality(item)
+                    "Backstage passes to a TAFKAL80ETC concert" -> item.quality = 0
+                    "Sulfuras, Hand of Ragnaros" -> {}
+                    else -> decreaseQuality(item)
                 }
             }
         }
