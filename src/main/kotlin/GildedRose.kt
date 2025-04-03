93ac8012de7159be8e04e161a21dd625a6433473
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-04-03 12:18:31 +0200

    Refactoring 2. Replace simple assignment with operator-assignment

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -5,23 +5,23 @@ class GildedRose(var items: List<Item>) {
             if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality > 0) {
                     if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
+                        item.quality -= 1
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
 
                     if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
                     }
@@ -29,7 +29,7 @@ class GildedRose(var items: List<Item>) {
             }
 
             if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
+                item.sellIn -= 1
             }
 
             if (item.sellIn < 0) {
@@ -37,15 +37,15 @@ class GildedRose(var items: List<Item>) {
                     if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.quality > 0) {
                             if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
+                                item.quality -= 1
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality
+                        item.quality -= item.quality
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality += 1
                     }
                 }
             }
