7fcfd7664537649eb9569a4bc3e816f5d80c78ba
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-04-03 12:32:11 +0200

    Refactoring 4. Split if condition. Added test case to keep branch coverage at 100%

--- src/test/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/GildedRoseTest.kt
@@ -27,6 +27,7 @@ class GildedRoseTest {
             Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20), Item("Backstage passes to a TAFKAL80ETC concert", -1, 0)),
             Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 20, 20), Item("Backstage passes to a TAFKAL80ETC concert", 19, 21)),
             Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 5, 49), Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)),
+            Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 5, 50), Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)),
             Arguments.of(Item("foo", 0, 2), Item("foo", -1, 0)),
             Arguments.of(Item("foo", 0, 0), Item("foo", -1, 0))
         )
