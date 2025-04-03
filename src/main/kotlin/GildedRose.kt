1b4b0e88b4bc5bf05e1e57c36ed790ccdd49ef9b
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-04-03 14:19:06 +0200

    Refactoring 11. 'when' conditions merged together

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -4,7 +4,13 @@ class GildedRose(var items: List<Item>) {
         items.stream().forEach { item ->
             // TODO next we will implement object updaters that we will later use in a factory method
             when (item.name) {
-                "Aged Brie" -> increaseQuality(item)
+                "Aged Brie" -> {
+                    increaseQuality(item)
+                    item.sellIn -= 1
+                    if (item.sellIn < 0) {
+                        increaseQuality(item)
+                    }
+                }
                 "Backstage passes to a TAFKAL80ETC concert" -> {
                     increaseQuality(item)
                     if (item.sellIn < 11) {
@@ -13,29 +19,15 @@ class GildedRose(var items: List<Item>) {
                     if (item.sellIn < 6) {
                         increaseQuality(item)
                     }
-                }
-                "Sulfuras, Hand of Ragnaros" -> {}
-                else -> decreaseQuality(item)
-            }
-
-            when (item.name) {
-                "Sulfuras, Hand of Ragnaros" -> {}
-                else -> item.sellIn -= 1
-            }
-
-            when (item.name) {
-                "Aged Brie" -> {
-                    if (item.sellIn < 0) {
-                        increaseQuality(item)
-                    }
-                }
-                "Backstage passes to a TAFKAL80ETC concert" -> {
+                    item.sellIn -= 1
                     if (item.sellIn < 0) {
                         item.quality = 0
                     }
                 }
                 "Sulfuras, Hand of Ragnaros" -> {}
                 else -> {
+                    decreaseQuality(item)
+                    item.sellIn -= 1
                     if (item.sellIn < 0) {
                         decreaseQuality(item)
                     }
