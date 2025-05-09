afb371df148f80bc943bcf654b676344dcf2a6ca
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-05-09 11:30:15 +0200

    Parametrisierte Tests

--- src/test/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/GildedRoseTest.kt
@@ -1,9 +1,32 @@
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class GildedRoseTest
+class GildedRoseTest {
+    @ParameterizedTest
+    @MethodSource("createItems") // This is the name of s static method
+    fun `parametrized example for update quality`(actual: Item, expected: Item) {
+        var gilde = GildedRose(listOf(actual))
+
+        gilde.updateQuality()
+        assertEquals(expected.toString(), gilde.items[0].toString())
+        // We create the GildedRose object and call the updateQuality method
+        // We test our assertions
+    }
+
+    companion object {
+        @JvmStatic
+        fun createItems() = listOf(
+            Arguments.of(Item("expected", 0, 0), Item("expected", -1, 0)),
+            Arguments.of(Item("Backstage passes to a TAFKAL80ETC concert", 1, 3), Item("Backstage passes to a TAFKAL80ETC concert", 0, 6)),
+            Arguments.of(Item("expected", 0, 3), Item("expected", -1, 1)),
+
+        )
+    }
+}
 
